3a3075b8c1961495d026579650c535de0385ef55
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-28 16:55:35 -0400

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * base-task <https://github.com/node-base/base-task>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
